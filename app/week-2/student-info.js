5acbcaf483d64615841fc3f27ab7b8aa9b5ae6ff
Rohan Sindhra <142118686+Rohan292004@users.noreply.github.com>
2024-01-29 13:07:45 -0700

    Week 2 assignment completed.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Shopping List</h1>
+      <h2>Rohan Sindhra</h2>
+      <h2><Link href="https://github.com/">"https://github.com/"</Link></h2>
+      
+    </div>
+  );
+}
