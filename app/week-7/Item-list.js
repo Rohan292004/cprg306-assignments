92171178c3294f11c85f474e65045eab034c2cd7
Rohan Sindhra <142118686+Rohan292004@users.noreply.github.com>
2024-03-20 07:11:18 -0600

    week-7

---
+++ app/week-7/Item-list.js
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useState } from 'react';
+import Item from './Item.js';
+
+const ItemList = ({ items }) => {
+    const [sortBy, setSortBy] = useState('name'); 
+    
+    const sortedItems = [...items].sort((a, b) => {
+      if (sortBy === 'name') {
+        return a.name.localeCompare(b.name);
+      } else if (sortBy === 'category') {
+        return a.category.localeCompare(b.category);
+      }
+      
+      return 0;
+    });
+  
+    
+    const handleSortByName = () => setSortBy('name');
+    const handleSortByCategory = () => setSortBy('category');
+  
+    return (
+      <div>
+        <button
+          onClick={handleSortByName}
+          style={{ backgroundColor: sortBy === 'name' ? 'green' : 'white' }}
+        >
+          Sort by Name
+        </button>
+        <button
+          onClick={handleSortByCategory}
+          style={{ backgroundColor: sortBy === 'category' ? 'green' : 'white' }}
+        >
+          Sort by Category
+        </button>
+  
+       
+        <ul>
+          {sortedItems.map(item => (
+            <Item key={item.name} name={item.name} quantity={item.quantity} category={item.category} />
+          ))}
+        </ul>
+      </div>
+    );
+};
+
+export default ItemList;
